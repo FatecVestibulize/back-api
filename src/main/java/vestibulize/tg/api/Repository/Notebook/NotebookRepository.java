15e8d790cf4b7f820a2070dd49289d0f204404e9
Gabriel Carvalho <gabrielrenatosc@gmail.com>
2025-10-06 23:49:04 -0300

    fix: ajusta consultas ao banco de dados

--- src/main/java/vestibulize/tg/api/Repository/Notebook/NotebookRepository.java
+++ src/main/java/vestibulize/tg/api/Repository/Notebook/NotebookRepository.java
@@ -2,9 +2,14 @@ package vestibulize.tg.api.Repository.Notebook;
 
 import vestibulize.tg.api.Entity.Notebook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface NotebookRepository extends JpaRepository<Notebook, Long>{
+
+    @Query(value = "SELECT * FROM notebook WHERE user_id = ?1 ORDER BY id DESC", nativeQuery = true)
+    List<Notebook> listByUserId(Long user_id);
 
 }
